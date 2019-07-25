b49fe5d88abb0312b0993570cf78488e7d700a2a
AghaZafeer <agha.zafeer@gmail.com>
2019-07-25 10:57:42 +0530

    task8

--- src/main/java/com/stackroute/boot/springbootdemotask/Runner.java
+++ src/main/java/com/stackroute/boot/springbootdemotask/Runner.java
@@ -1,11 +1,27 @@
 package com.stackroute.boot.springbootdemotask;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
 
+    //Environment Variable is Accessed
+    @Value("${spring.datasource.url}")
+    String s;
+    @Value("${seedTrack1.id}")
+    int id1;
+    @Value("${seedTrack1.name}")
+    String name1;
+    @Value("${seedTrack1.comment}")
+    String comment;
+    @Value("${seedTrack2.id}")
+    int id2;
+    @Value("${seedTrack2.name}")
+    String name2;
+    @Value("${seedTrack2.comment}")
+    String comment2;
 
     @Override
     public void run(String... args) throws Exception {
