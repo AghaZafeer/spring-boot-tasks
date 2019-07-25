ffeb74371434e3d4ec08f788a508846336a61f69
AghaZafeer <agha.zafeer@gmail.com>
2019-07-25 10:37:05 +0530

    task6

---
+++ src/main/java/com/stackroute/boot/springbootdemotask/Runner.java
@@ -0,0 +1,12 @@
+package com.stackroute.boot.springbootdemotask;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Runner implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.printf("Command Line Runner");
+    }
+}
